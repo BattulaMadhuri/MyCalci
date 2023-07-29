aee6136f4b7c8a38de9fc2ea78a46814c52a63bf
Madhuri Battula <137879232+BattulaMadhuri@users.noreply.github.com>
2023-07-29 15:57:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import './App.css';
+// //import Greet from './components/Greet';
+// //import Welcome from './components/Welcome';
+// import Message from './components/Message';
+
+// function App() {
+//   return (
+//     <div className="App"> 
+//     <Message />
+    
+//     {/* <Message />
+//      <Greet name="Madhu" heroName="Natasha" /> 
+//      <Greet name="Tarun" heroName="Iron Man"/> 
+//      <Greet name="Mahi" heroName="Thor"/> 
+//      <Welcome name="Madhu" /> */}
+
+//     </div>
+//   );
+// }
+
+// export default App;
+import Calc from './components/Calc';
+
+
+function App(){
+  return(
+    <Calc />
+  );
+}
+export default App;
